735612e0d4ac7dcd0e77e3611ed393af003052b8
mkamenet3 <maria.kamenetsky@gmail.com>
2019-05-22 23:45:05 -0500

    documentation for fake dataset

---
+++ R/SE_FakeData.R
@@ -0,0 +1,28 @@
+#'@title
+#'South-Eastern United Status - Fake Incidence Counts
+#'@description
+#'Fake dataset of disease incidece in South-Eastern United States. This dataset is used for illustration of the package in the vignette.
+#'
+#'@docType data
+#'
+#'@usage data(SE_FakeData)
+#'
+#'@format An object of class "data.frame". Includes the following variables:
+#'\itemize{
+#'    \item 1) State - Seven states of South-Eastern United States (Alabama, Florida, Georgia, Mississippi, North Carolina, South Carolina, and Tennessee); factor
+#'    \item 2) FIPS - County-level FIPS codes in South-Eastern United States
+#'    \item 3) long - longitude
+#'    \item 4) lat - latitude
+#'    \item 5) x - covariate
+#'    \item 6) y - response
+#'}
+#'
+#'@keywords datasets
+#'
+#'
+#'@aliases SE_FakeData
+#'
+#'@examples
+#'data(SE_FakeData)
+#'
+"SE_FakeData"
