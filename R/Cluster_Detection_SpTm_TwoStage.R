36e698321cc6fb2a4dc5724662a38a61dca4cc92
mkamenet3 <maria.kamenetsky@gmail.com>
2019-10-06 12:49:34 -0500

    added Matrix as a required package in readme

---
+++ R/Cluster_Detection_SpTm_TwoStage.R
@@ -0,0 +1,446 @@
+#Last Update: 2019/09/09
+###################################################################################################
+# setwd("C:/01.ACADEMY/03.UWM/SPATIAL/SpatialCluster/MyFunctions")
+#setwd("C:/JUNO/SpatialCluster/MyFunctions")
+source("FunctionMLC_LL_TMP.R")
+#source("FunctionMLC_LL.R")
+
+if (!require(Matrix)) {
+  install.packages("Matrix")
+  library(Matrix)
+}
+
+###################################################################################################
+# Test.MLC.TS.ID.ST1(yList, XList, cdataL, M, ID, overlap)                                    #####
+### Find and Test the Cylindrical Spatio-Temporal Cluster in the slopes for given ID
+###      via Two Stages Detection (in the 1st Stage)
+#### yList: The input data(in the List of Vectors Format)
+#### XList: The input data(in the List of Matrices Format)
+#### cdataL: Pre-defined cdata list which is from List.C.Data(DMatrix,MR)
+#### M : number of simulations
+#### ID: Indices for potential centroids
+#### overlap: Boolean which is TRUE for overlapping clusters / FALSE for non-overlapping clusters
+###################################################################################################
+Test.MLC.TS.ID.ST1 <- function(yList, XList, cdataL, M, ID, overlap) {
+  TestStat <- rep(NA,M+1)
+  N <- dim(XList[[1]])[1]; p <- dim(XList[[1]])[2]
+  T <- length(XList)
+  
+  MlcSlope <- TStg.CDL.ID.SL.ST(yList, XList, cdataL, ID, overlap)
+  Mlc <- MlcSlope$Mlc_slp
+  l <- length(Mlc)
+  sigSq1 <- MlcSlope$sigSq1_slp
+  # F statistic for the slope
+  TestStat[1]  <- MlcSlope$Mlc_slp[l]
+  
+  cent_tmp <- Mlc[1]
+  r_tmp <- Mlc[2]
+  d_tmp1 <- distm(cbind(long,lat), c(long[cent_tmp],lat[cent_tmp]))/1000
+  cluster  <- as.vector((d_tmp1 <= r_tmp))
+  
+  EyList <- list()
+  for (t in 1:T) {
+    yt <- yList[[t]]
+    Wt <- cbind(XList[[t]],cluster*1)
+    yt_valid <- yt[!is.na(yt)]
+    Wt_valid <- Wt[!is.na(yt),]
+    bt_tmp <- solve(t(Wt_valid)%*%Wt_valid)%*%t(Wt_valid)%*%yt_valid
+    EyList[[t]] <- Wt%*%bt_tmp
+  }
+  
+  for (k in 1:M) {
+    yList_k <- list()
+    for (t in 1:T) {
+      e_k <- rnorm(N, mean = 0, sd = sqrt(sigSq1))
+      e_k[is.na(yList[[t]])] <- NA
+      yList_k[[t]] <- EyList[[t]] + e_k	
+    }
+    TestStat[k+1] <- TStg.CDL.ID.SL.ST(yList_k, XList, cdataL, ID, overlap)$Mlc_slp[l]
+  }
+
+  pval <- (rank(-TestStat)[1])/(M+1)
+  #attributes(pval)$names <- "pval"
+  c(Mlc, pval=pval)
+}
+
+
+###################################################################################################
+# Find.Clusters.TS.ST1(yList, XList, long, lat, MR, M, overlap, alpha)                        #####
+### Find Multiple (Overlapping) Cylindrical Spatio-Temporal Clusters Sequentially in the slopes
+###      via Two Stages Detection (in the 1st Stage)
+#### yList: The input data(in the List of Vectors Format)
+#### XList: The input data(in the List of Matrices Format)
+#### long: longitude
+#### lat:  latitude
+#### M : number of simulations
+#### overlap: Boolean which is TRUE for overlapping clusters / FALSE for non-overlapping clusters
+#### alpha : significance level
+###################################################################################################
+Find.Clusters.TS.ST1 <- function(yList, XList, long, lat, MR, M, overlap, alpha) {
+  N <- dim(XList[[1]])[1]; p <- dim(XList[[1]])[2]
+  T <- length(XList)
+  ID <- 1:N
+  LL <- cbind(long, lat)
+  DMatrix <- distm(LL)/1000
+  cdataL <- List.C.Data(DMatrix,MR)
+  
+  print("Finding 1st Cluster")
+  time_tmp <- system.time(C_tmp <- Test.MLC.TS.ID.ST1(yList, XList, cdataL, M, ID, overlap))
+  # Clusters <- c(C_tmp,time_tmp[3]/60)
+  Clusters <- rbind(c(C_tmp,time_tmp[3]/60), NULL)    # Update: 2019/09/08
+  
+  l <- length(C_tmp)
+  pval_tmp <- C_tmp[l]
+  cent_tmp <- C_tmp[1]
+  r_tmp    <- C_tmp[2]
+  
+  clsL <- list()    # a list of indicator for each cluster
+  n_cls <- 1        # number of clusters
+  
+  d_tmp <- distm(cbind(long,lat), c(long[cent_tmp],lat[cent_tmp]))/1000
+  clsL[[n_cls]]  <- as.vector(d_tmp <= r_tmp)
+  ID_tmp <- ID[ID != cent_tmp]
+  
+  yList_tmp <- list()
+  for (t in 1:T) {
+    yt <- yList[[t]]
+    Xt <- XList[[t]]
+    Xt_cls <- Xt*(clsL[[n_cls]])
+    Wt <- cbind(Xt,Xt_cls)
+    yt_valid <- yt[!is.na(yt)]
+    Wt_valid <- Wt[!is.na(yt),]
+    bt_tmp <- solve(t(Wt_valid)%*%Wt_valid)%*%t(Wt_valid)%*%yt_valid
+    yList_tmp[[t]] <- yt - Xt_cls%*%bt_tmp[(p+1):(2*p)]
+  }
+  
+  while (pval_tmp < alpha) {
+    print(paste("Finding ", n_cls + 1, "th Cluster", sep=""))
+    time_tmp <- system.time(C_tmp <- Test.MLC.TS.ID.ST1(yList_tmp, XList, cdataL, M, ID_tmp, overlap))
+    Clusters <- rbind(Clusters,c(C_tmp,time_tmp[3]/60))
+    pval_tmp <- C_tmp[l]
+    cent_tmp <- C_tmp[1]
+    r_tmp    <- C_tmp[2]
+    
+    n_cls <- n_cls + 1
+    
+    d_tmp1 <- distm(cbind(long,lat), c(long[cent_tmp],lat[cent_tmp]))/1000
+    d_tmp2 <- distm(cbind(long[ID_tmp],lat[ID_tmp]), c(long[cent_tmp],lat[cent_tmp]))/1000
+    clsL[[n_cls]]  <- as.vector((d_tmp1 <= r_tmp))
+    ID_tmp <- ID_tmp[ID_tmp != cent_tmp]
+    
+    for (t in 1:T) {
+      yt <- yList_tmp[[t]]
+      Xt <- XList[[t]]
+      Xt_cls <- Xt*(clsL[[n_cls]])
+      Wt <- cbind(Xt,Xt_cls)
+      yt_valid <- yt[!is.na(yt)]
+      Wt_valid <- Wt[!is.na(yt),]
+      bt_tmp <- solve(t(Wt_valid)%*%Wt_valid)%*%t(Wt_valid)%*%yt_valid
+      yList_tmp[[t]] <- yt - Xt_cls%*%bt_tmp[(p+1):(2*p)]
+    }
+  }
+  
+  n_obs <- sapply(clsL, sum)
+  # if (length(n_obs) > 1) {
+  #   rownames(Clusters) <- NULL
+  #   Clusters <- cbind(Clusters, n_obs=n_obs)
+  # } else {
+  #   Clusters <- c(Clusters, n_obs=n_obs)
+  # }
+  rownames(Clusters) <- NULL                   # Update: 2019/09/08
+  Clusters <- cbind(Clusters, n_obs=n_obs)     # Update: 2019/09/08
+  # Clusters <- rbind(Clusters[,-3], NULL)
+  colnames(Clusters)[3] <- "TestStat"          # Update: 2019/09/08 
+  
+  # Update: 2019/09/09
+  Coef <- Est.Coeff.TS.ST(yList, XList, Cls1stIndicator=clsL, Cls2ndIndicator=clsL)
+  
+  return(list(Clusters = Clusters, Coef = Coef, Indicator = clsL))
+}
+
+
+###################################################################################################
+# Test.MLC.TS.ID.ST2(yList, XList, cdataL, M, ID, overlap)                                    #####
+### Find and Test the Cylindrical Spatio-Temporal Cluster in the intercepts for given ID
+###      via Two Stages Detection (in the 2nd Stage)
+#### yList: The input data(in the List of Vectors Format)
+#### XList: The input data(in the List of Matrices Format)
+#### cdataL: Pre-defined cdata list which is from List.C.Data(DMatrix,MR)
+#### M : number of simulations
+#### ID: Indices for potential centroids
+#### overlap: Boolean which is TRUE for overlapping clusters / FALSE for non-overlapping clusters
+###################################################################################################
+Test.MLC.TS.ID.ST2 <- function(yList, XList, cdataL, M, ID, overlap) {
+  TestStat <- rep(NA,M+1)
+  N <- dim(XList[[1]])[1]; p <- dim(XList[[1]])[2]
+  T <- length(XList)
+  
+  MlcSlope <- TStg.CDL.ID.SL.ST(yList, XList, cdataL, ID, overlap)
+  Mlc <- MlcSlope$Mlc_int
+  l <- length(Mlc)
+  sigSq1 <- MlcSlope$sigSq1_int
+  # F statistic for the slope
+  TestStat[1]  <- MlcSlope$Mlc_int[l]
+  
+  cent_tmp <- Mlc[1]
+  r_tmp <- Mlc[2]
+  d_tmp1 <- distm(cbind(long,lat), c(long[cent_tmp],lat[cent_tmp]))/1000
+  cluster  <- as.vector((d_tmp1 <= r_tmp))
+  
+  EyList <- list()
+  for (t in 1:T) {
+    yt <- yList[[t]]
+    Wt <- cbind(XList[[t]],XList[[t]][,2]*cluster)
+    yt_valid <- yt[!is.na(yt)]
+    Wt_valid <- Wt[!is.na(yt),]
+    bt_tmp <- solve(t(Wt_valid)%*%Wt_valid)%*%t(Wt_valid)%*%yt_valid
+    EyList[[t]] <- Wt%*%bt_tmp
+  }
+  
+  for (k in 1:M) {
+    yList_k <- list()
+    for (t in 1:T) {
+      e_k <- rnorm(N, mean = 0, sd = sqrt(sigSq1))
+      e_k[is.na(yList[[t]])] <- NA
+      yList_k[[t]] <- EyList[[t]] + e_k	
+    }
+    TestStat[k+1] <- TStg.CDL.ID.SL.ST(yList_k, XList, cdataL, ID, overlap)$Mlc_int[l]
+  }
+  
+  pval <- (rank(-TestStat)[1])/(M+1)
+  #attributes(pval)$names <- "pval"
+  c(Mlc, pval=pval)
+}
+
+
+###################################################################################################
+# Find.Clusters.TS.ST2(yList, XList, long, lat, MR, M, Cls1st, overlap, alpha)                #####
+### Find Multiple (Overlapping) Cylindrical Spatio-Temporal Clusters Sequentially in the intercepts
+###      via Two Stages Detection (in the 2nd Stage)
+#### yList: The input data(in the List of Vectors Format)
+#### XList: The input data(in the List of Matrices Format)
+#### long: longitude
+#### lat:  latitude
+#### M : number of simulations
+#### Cls1st: the output from "Find.Clusters.TS.ST1" in the 1st Stage
+#### overlap: Boolean which is TRUE for overlapping clusters / FALSE for non-overlapping clusters
+#### alpha : significance level
+###################################################################################################
+Find.Clusters.TS.ST2 <- function(yList, XList, long, lat, MR, M, Cls1st, overlap, alpha) {
+  N <- dim(XList[[1]])[1]; p <- dim(XList[[1]])[2]
+  T <- length(XList)
+  ID <- 1:N
+  LL <- cbind(long, lat)
+  DMatrix <- distm(LL)/1000
+  cdataL <- List.C.Data(DMatrix,MR)
+  
+  clsL <- Cls1st$Indicator
+  n_cls <- length(clsL) - 1       # number of clusters
+  n_cls1<- n_cls                  # number of significant clusters in the 1st-Stage / Update: 2015/06/19
+  
+  ID_tmp <- ID
+  yList_tmp <- yList
+  WList <- XList
+  if (n_cls1 > 0) {
+    for (j in 1:n_cls1) {
+      cent_tmp <- Cls1st$Clusters[j,1]
+      r_tmp <- Cls1st$Clusters[j,2]
+      
+      d_tmp1 <- distm(cbind(long,lat), c(long[cent_tmp],lat[cent_tmp]))/1000
+      d_tmp2 <- distm(cbind(long[ID_tmp],lat[ID_tmp]), c(long[cent_tmp],lat[cent_tmp]))/1000
+      ID_tmp <- ID_tmp[ID_tmp != cent_tmp]
+      for (t in 1:T) {
+        WList[[t]] <- cbind(WList[[t]],XList[[t]]*(clsL[[j]]))
+      }
+    }
+    
+    for (t in 1:T) {
+      yt <- yList_tmp[[t]]
+      Wt <- WList[[t]]
+      yt_valid <- yt[!is.na(yt)]
+      Wt_valid <- Wt[!is.na(yt),]
+      bt_tmp <- solve(t(Wt_valid)%*%Wt_valid)%*%t(Wt_valid)%*%yt_valid
+      yList_tmp[[t]] <- yt - Wt[,(p+1):length(bt_tmp)]%*%bt_tmp[(p+1):length(bt_tmp)]
+    }
+  }
+  
+  pval_tmp <- 0
+  Clusters <- NULL
+  while (pval_tmp < alpha) {
+    print(paste("Finding ", n_cls + 1, "th Cluster", sep=""))
+    time_tmp <- system.time(C_tmp <- Test.MLC.TS.ID.ST2(yList_tmp, XList, cdataL, M, ID_tmp, overlap))
+    Clusters <- rbind(Clusters,c(C_tmp,time_tmp[3]/60))
+    l <- length(C_tmp)
+    pval_tmp <- C_tmp[l]
+    cent_tmp <- C_tmp[1]
+    r_tmp    <- C_tmp[2]
+    
+    n_cls <- n_cls + 1
+    
+    d_tmp1 <- distm(cbind(long,lat), c(long[cent_tmp],lat[cent_tmp]))/1000
+    d_tmp2 <- distm(cbind(long[ID_tmp],lat[ID_tmp]), c(long[cent_tmp],lat[cent_tmp]))/1000
+    clsL[[n_cls]]  <- as.vector((d_tmp1 <= r_tmp))
+    ID_tmp <- ID_tmp[ID_tmp != cent_tmp]
+    
+    for (t in 1:T) {
+      yt <- yList_tmp[[t]]
+      Xt <- XList[[t]]
+      Xt_cls <- Xt*(clsL[[n_cls]])
+      Wt <- cbind(Xt,Xt_cls)
+      yt_valid <- yt[!is.na(yt)]
+      Wt_valid <- Wt[!is.na(yt),]
+      bt_tmp <- solve(t(Wt_valid)%*%Wt_valid)%*%t(Wt_valid)%*%yt_valid
+      yList_tmp[[t]] <- yt - Xt_cls%*%bt_tmp[(p+1):(2*p)]
+    }
+  }
+  
+  if (n_cls1 > 0) {
+    n_obs <- sapply(clsL, sum)[-(1:n_cls1)]
+  } else {
+    n_obs <- sapply(clsL, sum)
+  }
+  
+  # if (length(n_obs) > 1) {
+  #   rownames(Clusters) <- NULL
+  #   Clusters <- cbind(Clusters, n_obs=n_obs)
+  # } else {
+  #   Clusters <- c(Clusters, n_obs=n_obs)
+  # }
+  rownames(Clusters) <- NULL                   # Update: 2019/09/08
+  Clusters <- cbind(Clusters, n_obs=n_obs)     # Update: 2019/09/08
+  
+  n_cls2 <- n_cls - n_cls1 - 1                 # Update: 2019/09/08 
+  if (n_cls2 > 0) {                            # Update: 2019/09/08 
+    if (n_cls1 > 0) {
+      Clusters <- rbind(Cls1st$Clusters[1:n_cls1,], Clusters)
+    }
+    Clusters <- cbind(Clusters, stage=c(rep(1,n_cls1),rep(2,(n_cls-n_cls1))))
+  } else {
+    Clusters <- cbind(Cls1st$Clusters, stage=1)
+  }
+  
+  Clusters <- rbind(Clusters[,-3], NULL)      # Update: 2019/09/08 
+  
+  # Update: 2019/09/09
+  Coef <- Est.Coeff.TS.ST(yList, XList, Cls1stIndicator=Cls1st$Indicator, Cls2ndIndicator=clsL)
+  
+  return(list(Clusters = Clusters, Coef = Coef, Indicator = clsL))
+}
+
+###################################################################################################
+# Fit.Model.Clusters.ST                                                                       #####
+### lm Fit the model with detected clusters
+### Clusters: the output from "Find.Clusters.TS.ST1" and "Find.Clusters.TS.ST2"
+###################################################################################################
+Fit.Model.Clusters.TS.ST <- function(yList, XList, Cls1st, Cls2nd) {
+  WList <- XList
+  clsL <- Cls2nd$Indicator
+  n_cls1 <- length(Cls1st$Indicator) - 1       # number of clusters in the 1st Stage
+  n_cls2 <- length(clsL) - n_cls1- 1              # number of clusters in the 2nd Stage
+  n_cls <- n_cls1 + n_cls2
+  p <- dim(XList[[1]])[2]; T <- length(XList)
+  
+  coeff_namesList <- list()
+  modelList <- list()
+  for (t in 1:T) {
+    coeff_namesList[[t]] <- paste("b0_", 0:(p-1), "_t", t, sep="")   # names for the prameter estimates
+    modelList[[t]] <- paste(" + b0_", 0:(p-1), "_t", t, sep="", collapse = "")  # model to fit  
+  }
+
+  if (n_cls > 0) { 
+    for (j in 1:n_cls) {
+      for (t in 1:T) {
+        coeff_namesList[[t]][(p*j+1):(p*j+p)] <- paste("c", j, "_", 0:(p-1), "_t", t, sep="")
+        modelList[[t]] <- paste(modelList[[t]], paste(" + c", j, "_", 0:(p-1), "_t", t, sep="", collapse = ""), sep="")
+        WList[[t]] <- cbind(WList[[t]],XList[[t]]*(clsL[[j]]))
+      }
+    }
+  }
+  
+  coeff_names <- NULL
+  model <- "y ~ -1"
+  y <- NULL
+  W <- 181818
+  for (t in 1:T) {
+    coeff_names <- c(coeff_names, coeff_namesList[[t]])
+    model <- paste(model, modelList[[t]], sep="")
+    y <- c(y,yList[[t]])
+    W <- as.matrix(bdiag(W,WList[[t]]))
+  }
+  W <- W[-1,-1]
+  
+  data_cls <- data.frame(y,W)
+  colnames(data_cls) <- c("y", coeff_names)
+  lm(model, data = data_cls)
+}
+
+###################################################################################################
+# Est.Coeff.TS.ST(yList, XList, Cls1stIndicator, Cls2ndIndicator)                             #####
+###################################################################################################
+Est.Coeff.TS.ST <- function(yList, XList, Cls1stIndicator, Cls2ndIndicator) {
+  WList <- XList
+  clsL <- Cls2ndIndicator
+  n_cls1 <- length(Cls1stIndicator) - 1       # number of clusters in the 1st Stage
+  # n_cls2 <- length(clsL) - n_cls1- 1        # number of clusters in the 2nd Stage
+  n_cls2 <- length(clsL) - n_cls1             # Update: 2019/09/09
+  p <- dim(XList[[1]])[2]; T <- length(XList)
+  
+  b_tmp <- list()
+  coef_tmp <- list()
+  for (t in 1:T) {
+    b_tmp[[t]] <- solve(t(XList[[t]])%*%XList[[t]])%*%t(XList[[t]])%*%yList[[t]]
+    coef_tmp[[t]] <- c(b_tmp[[t]],rep(NA,length(b_tmp[[t]])))
+  }  
+  
+  XList_cls <- list()
+  yList_tmp <- yList
+  if (n_cls1 > 0) {
+    for (j in 1:n_cls1) {
+      for (t in 1:T) {
+        XList_cls[[t]] <- XList[[t]]*(clsL[[j]])
+        WList[[t]] <- cbind(XList[[t]],XList_cls[[t]])	
+        b_tmp[[t]] <- solve(t(WList[[t]])%*%WList[[t]])%*%t(WList[[t]])%*%yList_tmp[[t]]
+        coef_tmp[[t]] <- cbind(coef_tmp[[t]], b_tmp[[t]])	
+        yList_tmp[[t]] <- yList_tmp[[t]] - XList_cls[[t]]%*%b_tmp[[t]][(p+1):(2*p)]	
+      }
+    }
+  }
+  
+  if (n_cls2 > 0) {
+    for (j in 1:n_cls2) {
+      for (t in 1:T) {
+        WList[[t]] <- cbind(XList[[t]],1*(clsL[[j+n_cls1]]))
+        b_tmp[[t]] <- solve(t(WList[[t]])%*%WList[[t]])%*%t(WList[[t]])%*%yList_tmp[[t]]
+        coef_tmp[[t]] <- cbind(coef_tmp[[t]], c(b_tmp[[t]],0))	
+        yList_tmp[[t]] <- yList_tmp[[t]] - (clsL[[j+n_cls1]])*b_tmp[[t]][(p+1)]	
+      }
+    }
+  }
+  
+  n_cls <- n_cls1 + n_cls2
+  Coef <- list()
+  for (t in 1:T) {
+    Coef[[t]] <- cbind(coef_tmp[[t]][1:p,1:n_cls],NULL)
+    coef_names <- c("beta_0","beta_1")
+    if (n_cls > 1) {                             # Update: 2019/09/07
+      for (j in 1:(n_cls-1)) {
+        Coef[[t]] <- rbind(Coef[[t]],
+                           cbind(matrix(rep(NA,p*j),p,j), 
+                                 matrix(rep(coef_tmp[[t]][(p+1):(2*p),(j+1)],(n_cls-j)),p,(n_cls-j))))
+        coef_names <- c(coef_names, paste("theta_", j, ",", 0:(p-1), sep=""))
+      }
+    }
+    # colnames(Coef[[t]]) <- 0:n_cls
+    colnames(Coef[[t]]) <- 0:(n_cls-1)      # Update: 2019/09/07
+    rownames(Coef[[t]]) <- coef_names
+  }
+  
+  Coeff_Table <- NULL
+  for (t in 1:length(Coef)) {
+    Coeff_Table <- cbind(Coeff_Table, Coef[[t]][,(dim(Coef[[t]])[2])])  
+  }
+  
+  return(list(Coeff_History = Coef, Coeff_Table = Coeff_Table))
+}
+
+###################################################################################################
