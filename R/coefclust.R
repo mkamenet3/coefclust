b6f6441157897105de96ba9d4182deec3c746057
mkamenet3 <maria.kamenetsky@gmail.com>
2019-10-08 09:00:44 -0500

    deleted old code

--- R/coefclust.R
+++ R/coefclust.R
@@ -79,62 +79,6 @@ cum.sum.scalar2 <- function(yc, xc) {
 }
 
 
-
-#' DC.Simul.SL <- function(y, X, cdataL, ID, overlap) {
-#'   mlc <- NULL
-#'   N <- dim(X)[1]; p <- dim(X)[2]
-#'
-#'   # compute sum(y_i), sum(y_i^2), sum(x_i), sum(x_i^2), sum(x_i*y_i)
-#'   x <- X[,p]
-#'   sum_y1 <- sum(y);  sum_y2 <- sum(y^2)
-#'   sum_x1 <- sum(x);  sum_x2 <- sum(x^2)
-#'   sum_xy <- sum(x*y)
-#'
-#'   wholeID <- 1:N
-#'   nonID <- wholeID[!(wholeID %in% ID)]
-#'
-#'   for (k in ID) {
-#'     dat.k <- cdataL[[k]]
-#'     dat.k$dr <- round(dat.k$r - c(dat.k$r[-1],0),10)
-#'     # For non-overlapping assumpsion
-#'     if (!overlap && length(which(dat.k$id %in% nonID)) > 0) {
-#'       # remove all cluster which overlaps the previous clusters
-#'       # also remove all cluster which is greater than overlapped one
-#'       dat.k <- dat.k[-(min(which(dat.k$id %in% nonID)):length(dat.k$id)),]
-#'     }
-#'
-#'     if (!is.null(dim(dat.k)) && dim(dat.k)[1] > p) {
-#'       yc <- y[dat.k$id]; xc <- x[dat.k$id]
-#'
-#'       # compute cumulative sum
-#'       cs <- cum.sum.scalar(yc, xc)
-#'
-#'       # compute N*sigSq hat
-#'       dat.k$Nsig2.hat<- sum_y2 - (sum_y1-cs$y1)^2/(N-cs$nc) - (cs$y1)^2/(cs$nc) -
-#'         ((sum_xy-cs$xy) - (sum_x1-cs$x1)*(sum_y1-cs$y1)/(N-cs$nc))^2/((sum_x2-cs$x2) - (sum_x1-cs$x1)^2/(N-cs$nc)) -
-#'         ((cs$xy) - (cs$x1)*(cs$y1)/(cs$nc))^2/((cs$x2) - (cs$x1)^2/(cs$nc))
-#'
-#'       # Remove -Inf which is in case of all x's are the same
-#'       dat.k2 <- as.matrix(dat.k[dat.k$dr!=0 & dat.k$Nsig2.hat > 0,])
-#'       dim1 <- dim(dat.k2)[1]
-#'       dim2 <- dim(dat.k2)[2]
-#'
-#'       # Pick the smallest one. First p-1 is NA or NaN
-#'       kpick <- order(dat.k2[,dim2])[1]
-#'       kpick1 <- c(k, dat.k2[kpick,dim2-2], dat.k2[kpick,dim2]/N)
-#'       mlc <- rbind(mlc,kpick1)
-#'     }
-#'   }
-#'
-#'   colnames(mlc) <- c("center","radius","sig2.hat")
-#'   d2 <- dim(mlc)[2]
-#'
-#'   # Pick the smallest one
-#'   Mlc <- mlc[order(mlc[,d2])[1],]
-#'
-#'   return(list(Mlc = Mlc, mlc = mlc))
-#' }
-
 #' @title MLC.CDL.ID.SL
 #' @description Find the most likely cluster in the simple linear regression for given centroids.
 #' @param y The input data (in vector format).
@@ -194,65 +138,6 @@ MLC.CDL.ID.SL <- function(y, X, cdataL, ID, overlap) {
 }
 
 
-
-#' DC.Simul.SL2 <- function(y, X, cdataL, sum_x1, sum_x2, cs.xxL, ID, overlap) {
-#'   mlc <- NULL
-#'   N <- length(y); p <- 2
-#'
-#'   # compute sum(y_i), sum(y_i^2), sum(x_i*y_i)
-#'   sum_y1 <- sum(y);  sum_y2 <- sum(y^2)
-#'   sum_xy <- sum(X*y)
-#'
-#'   wholeID <- 1:N
-#'   nonID <- wholeID[!(wholeID %in% ID)]
-#'
-#'   for (k in ID) {
-#'     dat.k <- cdataL[[k]]
-#'     dat.k$dr <- round(dat.k$r - c(dat.k$r[-1],0),10)
-#'     # For non-overlapping assumpsion
-#'     if (!overlap && length(which(dat.k$id %in% nonID)) > 0) {
-#'       # remove all cluster which overlaps the previous clusters
-#'       # also remove all cluster which is greater than overlapped one
-#'       dat.k <- dat.k[-(min(which(dat.k$id %in% nonID)):length(dat.k$id)),]
-#'     }
-#'
-#'     if (!is.null(dim(dat.k)) && dim(dat.k)[1] > p) {
-#'       yc <- y[dat.k$id]; xc <- X[dat.k$id]
-#'
-#'       # compute cumulative sum
-#'       tmp<- cs.xxL[[k]]
-#'       cs <- cum.sum.scalar2(yc, xc)
-#'       x1 <- tmp$x1[1:length(yc)]
-#'       x2 <- tmp$x2[1:length(yc)]
-#'       nc <- tmp$nc[1:length(yc)]
-#'
-#'       # compute N*sigSq hat
-#'       dat.k$Nsig2.hat<- sum_y2 - (sum_y1-cs$y1)^2/(N-nc) - (cs$y1)^2/(nc) -
-#'         ((sum_xy-cs$xy) - (sum_x1-x1)*(sum_y1-cs$y1)/(N-nc))^2/((sum_x2-x2) - (sum_x1-x1)^2/(N-nc)) -
-#'         ((cs$xy) - (x1)*(cs$y1)/(nc))^2/((x2) - (x1)^2/(nc))
-#'
-#'       # Remove -Inf which is in case of all x's are the same
-#'       dat.k2 <- as.matrix(dat.k[dat.k$dr!=0 & dat.k$Nsig2.hat > 0,])
-#'       dim1 <- dim(dat.k2)[1]
-#'       dim2 <- dim(dat.k2)[2]
-#'
-#'       # Pick the smallest one. First p-1 is NA or NaN
-#'       kpick <- order(dat.k2[,dim2])[1]
-#'       kpick1 <- c(k, dat.k2[kpick,dim2-2], dat.k2[kpick,dim2]/N)
-#'       mlc <- rbind(mlc,kpick1)
-#'     }
-#'   }
-#'
-#'   colnames(mlc) <- c("center","radius","sig2.hat")
-#'   d2 <- dim(mlc)[2]
-#'
-#'   # Pick the smallest one
-#'   Mlc <- mlc[order(mlc[,d2])[1],]
-#'
-#'   return(list(Mlc = Mlc, mlc = mlc))
-#' }
-#'
-#'
 #'
 #' @title MLC.CDL2.ID.SL
 #' @description Find the most likely cluster in the simple linear regression for given potential centroids.
@@ -323,70 +208,6 @@ MLC.CDL2.ID.SL <- function(y, x, cdataL, sum_x1, sum_x2, cs.xxL, ID, overlap) {
     Mlc <- mlc[order(mlc[,d2])[1],]
     return(list(Mlc = Mlc, mlc = mlc))
 }
-
-#' DC.TStg1.SL <- function(y, X, cdataL, ID, overlap) {
-#'   mlc <- NULL
-#'   N <- dim(X)[1]; p <- dim(X)[2]
-#'
-#'   # compute sum(y_i), sum(y_i^2), sum(x_i), sum(x_i^2), sum(x_i*y_i)
-#'   x <- X[,p]
-#'   sum_y1 <- sum(y);  sum_y2 <- sum(y^2)
-#'   sum_x1 <- sum(x);  sum_x2 <- sum(x^2)
-#'   sum_xy <- sum(x*y)
-#'
-#'   wholeID <- 1:N
-#'   nonID <- wholeID[!(wholeID %in% ID)]
-#'
-#'   for (k in ID) {
-#'     dat.k <- cdataL[[k]]
-#'     dat.k$dr <- round(dat.k$r - c(dat.k$r[-1],0),10)
-#'     # For non-overlapping assumpsion
-#'     if (!overlap && length(which(dat.k$id %in% nonID)) > 0) {
-#'       # remove all cluster which overlaps the previous clusters
-#'       # also remove all cluster which is greater than overlapped one
-#'       dat.k <- dat.k[-(min(which(dat.k$id %in% nonID)):length(dat.k$id)),]
-#'     }
-#'
-#'     if (!is.null(dim(dat.k)) && dim(dat.k)[1] > p) {
-#'       yc <- y[dat.k$id]; xc <- x[dat.k$id]
-#'
-#'       # compute cumulative sum
-#'       cs <- cum.sum.scalar(yc, xc)
-#'
-#'       # compute N*sigSq hat
-#'       dat.k$Nsig2.hat<- sum_y2 - (sum_y1-cs$y1)^2/(N-cs$nc) - (cs$y1)^2/(cs$nc) -
-#'         ((sum_xy-cs$xy) - (sum_x1-cs$x1)*(sum_y1-cs$y1)/(N-cs$nc))^2/((sum_x2-cs$x2) - (sum_x1-cs$x1)^2/(N-cs$nc)) -
-#'         ((cs$xy) - (cs$x1)*(cs$y1)/(cs$nc))^2/((cs$x2) - (cs$x1)^2/(cs$nc))
-#'
-#'       # compute theta1
-#'       theta1 <- ((cs$xy) - (cs$x1)*(cs$y1)/(cs$nc))/((cs$x2) - (cs$x1)^2/(cs$nc)) -
-#'         ((sum_xy-cs$xy) - (sum_x1-cs$x1)*(sum_y1-cs$y1)/(N-cs$nc))/((sum_x2-cs$x2) - (sum_x1-cs$x1)^2/(N-cs$nc))
-#'
-#'       # compute Fstat.coeff
-#'       dat.k$Fstat.1 <- (theta1^2)/(1/((cs$x2) - (cs$x1)^2/(cs$nc)) + 1/((sum_x2-cs$x2) - (sum_x1-cs$x1)^2/(N-cs$nc)))/
-#'         ((dat.k$Nsig2.hat)/(N-2*p))
-#'
-#'       # Remove -Inf which is in case of all x's are the same
-#'       dat.k2 <- as.matrix(dat.k[dat.k$dr!=0 & dat.k$Nsig2.hat > 0,])  # Update: 2015/11/11
-#'
-#'       dim1 <- dim(dat.k2)[1]
-#'       dim2 <- dim(dat.k2)[2]
-#'
-#'       # Pick the largest Fstat.1. First p-1 is NA or NaN
-#'       kpick <- order(-dat.k2[,dim2])[1]
-#'       mlc <- rbind(mlc, dat.k2[kpick,-c(2,4)])
-#'     }
-#'   }
-#'
-#'   mlc[,3] <- mlc[,3]/N
-#'   colnames(mlc) <- c("center","radius","sig2.hat","maxFstat.1")
-#'   d2 <- dim(mlc)[2]
-#'
-#'   # Pick the largest Fstat.1.
-#'   Mlc <- mlc[order(-mlc[,d2])[1],]
-#'
-#'   return(list(Mlc = Mlc, mlc = mlc))
-#' }
 
 #' @title Fstat.CDL.ID.SL1
 #' @description Find the most likely cluster in the simple linear regression for given potential centroids in the slope: different slope and different intercept.
@@ -461,64 +282,6 @@ Fstat.CDL.ID.SL1 <- function(y, X, cdataL, ID, overlap) {
     return(list(Mlc = Mlc, mlc = mlc))
 }
 
-#'
-#' DC.TStg2.SL <- function(y, X, cdataL, ID, overlap) {
-#'   mlc <- NULL
-#'   N <- dim(X)[1]; p <- dim(X)[2]
-#'
-#'   # compute sum(y_i), sum(y_i^2), sum(x_i), sum(x_i^2), sum(x_i*y_i)
-#'   x <- X[,p]
-#'   sum_y1 <- sum(y);  sum_y2 <- sum(y^2)
-#'   sum_x1 <- sum(x);  sum_x2 <- sum(x^2)
-#'   sum_xy <- sum(x*y)
-#'
-#'   wholeID <- 1:N
-#'   nonID <- wholeID[!(wholeID %in% ID)]
-#'
-#'   for (k in ID) {
-#'     dat.k <- cdataL[[k]]
-#'     dat.k$dr <- round(dat.k$r - c(dat.k$r[-1],0),10)
-#'     # For non-overlapping assumpsion
-#'     if (!overlap && length(which(dat.k$id %in% nonID)) > 0) {
-#'       # remove all cluster which overlaps the previous clusters
-#'       # also remove all cluster which is greater than overlapped one
-#'       dat.k <- dat.k[-(min(which(dat.k$id %in% nonID)):length(dat.k$id)),]
-#'     }
-#'
-#'     if (!is.null(dim(dat.k)) && dim(dat.k)[1] >= p) {  # the same slope / different intercept
-#'       yc <- y[dat.k$id]; xc <- x[dat.k$id]
-#'
-#'       # compute cumulative sum
-#'       cs <- cum.sum.scalar(yc, xc)
-#'
-#'       # compute N*sigSq hat
-#'       dat.k$Nsig2.hat<- sum_y2 -
-#'         (((cs$nc)*sum_x2-cs$x1^2)*sum_y1^2 + (cs$nc)*(N-(cs$nc))*sum_xy^2 +
-#'            (N*sum_x2-sum_x1^2)*cs$y1^2 - 2*(cs$nc)*(sum_x1-cs$x1)*sum_y1*sum_xy -
-#'            2*((cs$nc)*sum_x2-sum_x1*cs$x1)*sum_y1*cs$y1 +
-#'            2*((cs$nc)*sum_x1-N*cs$x1)*sum_xy*cs$y1) /
-#'         ((cs$nc)*((N-(cs$nc))*(sum_x2-cs$x1^2/(cs$nc)) - (sum_x1-cs$x1)^2))
-#'
-#'       # Remove -Inf which is in case of all x's are the same
-#'       dat.k2 <- as.matrix(dat.k[dat.k$dr!=0 & dat.k$Nsig2.hat > 0,])
-#'       dim1 <- dim(dat.k2)[1]
-#'       dim2 <- dim(dat.k2)[2]
-#'
-#'       # Pick the smallest one. First p-1 is NA or NaN
-#'       kpick <- order(dat.k2[,dim2])[1]
-#'       kpick1 <- c(k, dat.k2[kpick,dim2-2], dat.k2[kpick,dim2]/N)
-#'       mlc <- rbind(mlc,kpick1)
-#'     }
-#'   }
-#'
-#'   colnames(mlc) <- c("center","radius","sig2.hat")
-#'   d2 <- dim(mlc)[2]
-#'
-#'   # Pick the smallest one
-#'   Mlc <- mlc[order(mlc[,d2])[1],]
-#'
-#'   return(list(Mlc = Mlc, mlc = mlc))
-#' }
 
 #' @title Fstat.CDL.ID.SL2
 #' @description Find the most likely cluster in the simple linear regression for given potential centroids in the intercept only: the same slope but different intercepts.
@@ -588,7 +351,7 @@ Fstat.CDL.ID.SL2 <- function(y, X, cdataL, ID, overlap) {
 
     return(list(Mlc = Mlc, mlc = mlc))
 }
-##############################################################################
+
 #'@title MLC.CDL.ID.1AOVbi
 #'@description Find the most likely cluster in the one-way ANOVA with a binary covariate for given potential centroids.
 #'@param y The input data (in vector format, 2N-by-1).
